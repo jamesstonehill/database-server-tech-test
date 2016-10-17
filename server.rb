f38ae5ddb81de17674ee176e5e874652dfc7e222
James Stonehill <jamesstonehill@gmail.com>
2016-10-17 11:55:41 +0100

    building get logic

--- server.rb
+++ server.rb
@@ -3,10 +3,29 @@ require 'sinatra/base'
 class DataServer < Sinatra::Base
   set :port, 4000
 
+  memory_data = {}
+
   get '/' do
     'Welcome to DataServer'
   end
 
-  run! if app_file == $0
+  post '/set' do
+    puts
+    puts "PARAMS"
+    puts params
+    memory_data.merge!(params)
+    puts "DATA"
+    puts memory_data
+    params #this is here so I can test the params
+  end
 
+  get '/get' do
+    'No key entered'
+  end
+
+  get '/get:key' do
+    memory_data[params['key']]
+  end
+
+  run! if app_file == $0
 end
--- server_spec.rb
+++ server_spec.rb
@@ -8,24 +8,43 @@ class DataServerTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
   def app
-    DataServer
+    DataServer.new
   end
 
   def test_it_says_data_server
-    puts 'RUNNING'
     get '/'
     assert last_response.ok?
     assert_equal 'Welcome to DataServer', last_response.body
   end
 
+  # ============= /set tests ===================
+
   def test_it_serves_set
-    get '/set'
+    post '/set'
     assert last_response.ok?
   end
+
+  def test_set_params
+    sample_params = {'somekey' => 'somevalue'}
+    post '/set', params=sample_params
+    assert last_response.ok?
+    puts "BODY ONE"
+    puts last_response.body
+    assert_equal last_response.body, "[\"somekey\", \"somevalue\"]"
+  end
+
+
+
+# ============= /get tests ===================
 
   def test_it_serves_get
     get '/get'
     assert last_response.ok?
+  end
+
+  def test_it_returns_error_message_without_any_data
+    get '/get'
+    assert_equal last_response.body, 'No key entered'
   end
 
 end
