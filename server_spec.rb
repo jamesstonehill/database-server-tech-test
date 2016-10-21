9efe55c65229e481056e2a631038dc123ad6b842
James Stonehill <jamesstonehill@gmail.com>
2016-10-21 10:32:28 +0100

    added test coverage

--- server_spec.rb
+++ server_spec.rb
@@ -38,16 +38,21 @@ class DataServerTest < Test::Unit::TestCase
   end
 
 
-# ============= /get tests ===================
+# ================ /get tests ===================
 
   def test_it_serves_get
     get '/get'
     assert last_response.ok?
   end
 
+  def tests_a_blank_key
+    get_value('')
+    assert_equal last_response.body, ''
+  end
+
   def test_a_single_request
     #tests when you submit an unknown key, it should return a blank string
-    get '/get?key=unknownkey'
+    get_value('unknownkey')
     assert_equal last_response.body, ''
   end
 
@@ -61,7 +66,7 @@ class DataServerTest < Test::Unit::TestCase
     my_value = "value1"
     get "/set?#{my_key}=#{my_value}"
 
-    get "/get?key=#{my_key}"
+    get_value(my_key)
     assert_equal last_response.body, "#{my_value}"
   end
 
@@ -71,19 +76,33 @@ class DataServerTest < Test::Unit::TestCase
     submit_key_value("key1", "value1")
     submit_key_value("key2", "value2")
 
-    get "/get?key=key1"
+    get_value("key1")
     assert_equal last_response.body, "value1"
 
-    get "/get?key=key2"
+    get_value("key2")
     assert_equal last_response.body, "value2"
+  end
+
+  def test_feature_3
+    #tests that a user can update a key-value pair
+
+    submit_key_value("key1", "value1")
+    submit_key_value("key1", "new value")
+
+    get_value("key1")
+    assert_equal last_response.body, "new value"
   end
 
   private
 
-# =========== Test helper methods ==================
+# ============= Test helper methods ==================
 
   def submit_key_value(key, value)
     get "/set?#{key}=#{value}"
+  end
+
+  def get_value(key)
+    get "/get?key=#{key}"
   end
 
 end
